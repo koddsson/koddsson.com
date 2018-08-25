5e355798296ece2f56d1118a990b3d7ea75f134a
Kristján Oddsson <koddsson@gmail.com>
2018-08-25 21:18:26 +0100

    add logging

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
+const app = express()
 app.use(express.static('public'))
+app.use(morgan('combined'))
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
