dd38abcbb5db4b218ab35f0a214fce70c8d3c67d
Kristján Oddsson <koddsson@gmail.com>
2018-08-26 00:25:32 +0100

    add logging middleware after asset middleware

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-app.use(express.static('public'))
 app.use(morgan('combined'))
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
