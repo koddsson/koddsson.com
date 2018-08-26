a8de2e9bc28d46666d16dab13854c8f2015679ec
Kristján Oddsson <koddsson@gmail.com>
2018-08-26 01:22:55 +0100

    actually send headers correctly

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ app.use(bodyParser.json())
 
 app.post('/micropub', async (req, res) => {
   const response = await fetch('https://tokens.indieauth.com/token', {
-    Accept: 'application/json',
-    Authorization: req.header('Authorization')
+    headers: {
+      Accept: 'application/json',
+      Authorization: req.header('Authorization')
+    }
   })
-  const json = await response.text()
+  const json = await response.json()
 
   console.log(req.body)
   console.log(json)
 
-  res.status(404)
+  res.status(404).send('Not found')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
