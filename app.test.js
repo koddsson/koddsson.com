8e3de7d7aadc95e5e7de5e152c163c94b06bf804
Kristján Oddsson <koddsson@gmail.com>
2018-09-18 19:24:08 +0100

    add tests for /favorites

--- app.test.js
+++ app.test.js
@@ -1,9 +1,16 @@
 const request = require('supertest');
 const app = require('./app')
 
-describe('Test the root path', () => {
-  test('It should response the GET method', (done) => {
+describe('GET', () => {
+  test('/', (done) => {
     request(app).get('/').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('/favorites', (done) => {
+    request(app).get('/favorites').then((response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
