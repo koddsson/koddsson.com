d18712c851b6efd86927c27ff538d34dfd95cacf
Kristján Oddsson <koddsson@gmail.com>
2019-12-20 13:35:55 +0000

    repect $PORT variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const app = require('./app')
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+const port = process.env.PORT
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
