dc6d01ad72bec13268d83f30de507ae2d6e88702
Kristján Oddsson <koddsson@gmail.com>
2023-03-22 20:54:57 +0100

    Give up my dreams of a 512kb website

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ My personal Website.
 Software dependencies are managed by npm as well as any build scripts.
 
 Install the dependencies with `npm install`, run the tests with `npm test` and run the app with `npm start`.
-
-## Useful links
-
-https://512kb.club/
--- lighthouserc.js
+++ lighthouserc.js
@@ -8,9 +8,7 @@ module.exports = {
     },
     assert: {
       "assertions": {
-        // Make sure all pages are below 512kb. See: https://512kb.club/
-        "resource-summary:total:size": ["error", {"maxNumericValue": 512000}],
-        "categories:performance": ["error", {"minScore": 0.9}],
+        "categories:performance": ["error", {"minScore": 0.95}],
         "categories:accessibility": ["error", {"minScore": 1}],
         "categories:best-practices": ["error", {"minScore": 1}],
         "categories:seo": ["error", {"minScore": 1}]
