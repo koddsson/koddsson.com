b370bda83da53180b3631de47ce2e82860d44e35
Kristján Oddsson <koddsson@gmail.com>
2023-05-16 21:09:10 +0200

    Wrap script in function

--- puppeteer.js
+++ puppeteer.js
@@ -1,12 +1,14 @@
-// Intercept API response and pass mock data for Puppeteer
-await page.setRequestInterception(true);
-page.on('request', request => {
-  if (request.url().startsWith('https://vitals.koddsson.workers.dev')) {
-    request.respond({
-      status: 201,
-      body: '' 
-    });
-  } else {
-    request.continue();
-  }
-});
+module.exports = async (browser, context) => {
+  // Intercept API response and pass mock data for Puppeteer
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.url().startsWith('https://vitals.koddsson.workers.dev')) {
+      request.respond({
+        status: 201,
+        body: '' 
+      });
+    } else {
+      request.continue();
+    }
+  });
+};
