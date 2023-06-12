1c8705789de35ef72be520835241e8ffc6e25fb2
Kristján Oddsson <koddsson@gmail.com>
2023-06-12 15:51:55 +0200

    Maybe fix request mocking

--- puppeteer.js
+++ puppeteer.js
@@ -1,21 +1,23 @@
 module.exports = async (browser, context) => {
-  // launch browser for LHCI
-  const page = await browser.newPage();
+  console.log('HELLO FROM PUPPETEER');
+  browser.on("targetcreated", async (target) => {
+    const page = await target.page();
 
-  // Intercept API response and pass mock data for Puppeteer
-  await page.setRequestInterception(true);
-  page.on('request', request => {
-    if (request.isInterceptResolutionHandled()) return;
-    if (request.url().startsWith('https://vitals.koddsson.workers.dev')) {
-      request.respond({
-        status: 201,
-        body: '' 
-      });
-    } else {
-      request.continue();
-    }
+    // Intercept API response and pass mock data for Puppeteer
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (request.isInterceptResolutionHandled()) return;
+      const url = request.url();
+      if (request.url().startsWith("https://vitals.koddsson.workers.dev")) {
+        console.log('HELLO FROM RESPONSE', url);
+        request.respond({
+          status: 201,
+          body: "",
+        });
+      } else {
+        console.log('HELLO FROM CONTINUE', url);
+        request.continue();
+      }
+    });
   });
-
-  // close session for next run
-  await page.close();
 };
