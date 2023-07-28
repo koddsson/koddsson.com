42515a08fd8a2deca496092c5537b804e6fc8ba9
Kristján Oddsson <koddsson@gmail.com>
2023-07-28 21:02:42 +0200

    remove console.log calls from puppeteer config

--- puppeteer.js
+++ puppeteer.js
@@ -1,5 +1,4 @@
 module.exports = async (browser, context) => {
-  console.log('HELLO FROM PUPPETEER');
   browser.on("targetcreated", async (target) => {
     const page = await target.page();
 
@@ -9,13 +8,11 @@ module.exports = async (browser, context) => {
       if (request.isInterceptResolutionHandled()) return;
       const url = request.url();
       if (request.url().startsWith("https://vitals.koddsson.workers.dev")) {
-        console.log('HELLO FROM RESPONSE', url);
         request.respond({
           status: 201,
           body: "",
         });
       } else {
-        console.log('HELLO FROM CONTINUE', url);
         request.continue();
       }
     });
